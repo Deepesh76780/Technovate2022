c28eab51c10aa82f8fd8a98a29c24a60c3731176
Rony <ronyrock1@gmail.com>
2022-07-20 23:52:31 +0530

    Footer links added

--- src/Mycomps/Navbar.jsx
+++ src/Mycomps/Navbar.jsx
@@ -7,26 +7,28 @@ export const Navbar = () => {
             <div className="h-16 w-screen flex flex-row justify-start items-center">
 
                 <div className='w-[40px]'></div>
-                <img className='h-[80px] w-[80px]' src="Images/Logo.svg" alt="Logo" />
+                <Link className='cursor-pointer' to="/">
+                    <img className='h-[80px] w-[80px]' src="Images/Logo.svg" alt="Logo" />
+                </Link>
 
                 <div
                     className="flex flex-row space-x-[3vw] text-red-100 text-[20px] spaced px-[10vw] ">
-                    <div>
+                    <div className='hover:text-[#E72D2D] transition-colors duration-300 cursor-pointer'>
                         REGISTER
                     </div>
-                    <Link to='/event'>
+                    <Link className='hover:text-[#E72D2D] transition-colors duration-300 cursor-pointer' to='/event'>
                         EVENT
                     </Link>
-                    <div>
+                    <div className='hover:text-[#E72D2D] transition-colors duration-300 cursor-pointer'>
                         SCHEDULE
                     </div>
-                    <div>
+                    <div className='hover:text-[#E72D2D] transition-colors duration-300 cursor-pointer'>
                         SPONSORS
                     </div>
-                    <Link to='/team'> 
+                    <Link className='hover:text-[#E72D2D] transition-colors duration-300 cursor-pointer' to='/team'>
                         TEAM
                     </Link>
-                    <Link to='/blog'>
+                    <Link className='hover:text-[#E72D2D] transition-colors duration-300 cursor-pointer' to='/blog'>
                         BLOGS
                     </Link>
                 </div>
