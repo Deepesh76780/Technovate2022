5d9bf5aa27f6e38226e625b6c2072969ce0f57a2
Rony <ronyrock1@gmail.com>
2022-07-29 03:17:50 +0530

    Team fixed

--- src/Mycomps/Team/TeamCard.jsx
+++ src/Mycomps/Team/TeamCard.jsx
@@ -1,19 +1,31 @@
 import React from 'react'
 
-export const TeamCard = ({name,role,phone,email}) => {
+export const TeamCard = ({ name,image, role, phone, email ,linkedin }) => {
     return (
         <div className='flex flex-col justify-center items-center relative'>
             <div className='z-0'>
-                <img src="./Images/Team/man.avif" className='w-48 h-48 md:h-60 md:w-60 rounded-2xl bg-cover' alt="" />
+                <img src={image} className='w-48 h-48 md:h-60 md:w-60 rounded-2xl bg-cover' alt="" />
             </div>
-            <div className='text-base md:text-xl tracking-widest mt-2'>
+            <div className='text-base tracking-widest mt-2'>
                 {name}
             </div>
             <div className='z-10 w-48 h-48 md:h-60 md:w-60 flex flex-col space-y-2 justify-center items-center rounded-2xl absolute top-0 bg-black bg-opacity-75 opacity-0 hover:opacity-100 transition-opacity duration-500 ease-in '>
-                <div className='text-base md:text-xl'>{role}</div>
-                <a className='text-xs md:text-base' href={`tel:${phone}`}>{phone}</a>
-                <a className='text-xs md:text-base' href={`mailto:${email}`}>{email}</a>
+            <div className='text-xs'>{role}</div>
+                <a className='text-base' href={`tel:${phone}`}>{phone}</a>
+                <div className='flex flex-row'>
+                    <a className='text-base' href={`mailto:${email}`}>
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" viewBox="0 0 20 20" fill="currentColor">
+  <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
+  <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
+</svg>
+                    </a>
+                    <a className='text-base ml-4' href={linkedin}>
+                        <img src="./Images/Team/linkedin2.svg" alt="" />
+                    </a>
+                </div>
             </div>
         </div>
     )
 }
+
+
