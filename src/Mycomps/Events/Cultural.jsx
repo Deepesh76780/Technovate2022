2ab9adb117552dfb11e3bc899205dac3db47afba
Rony <ronyrock1@gmail.com>
2022-08-28 15:44:20 +0530

    .htaccess file added for routing & cultural events added not published

--- src/Mycomps/Events/Cultural.jsx
+++ src/Mycomps/Events/Cultural.jsx
@@ -2,13 +2,24 @@ import React from 'react'
 import { EventCard } from './EventCard'
 
 export const Cultural = () => {
+
     const cultural = [
-        {
-            name: 'Cultural',
-            details: 'Rang Saari',
-            image: './Images/Event/cultural.png'
-        },
-        
+        { name: 'Rangotsav', image: './Images/Event/cultural.png' },
+
+        { name: 'Raag Ratri', image: './Images/Event/cultural.png' },
+
+        { name: 'What do you meme', image: './Images/Event/cultural.png' },
+
+        { name: "Let's Nacho", image: './Images/Event/cultural.png' },
+        { name: 'Nukkad Natak Contest', image: './Images/Event/cultural.png' },
+
+        { name: 'Battle of Bands', image: './Images/Event/cultural.png' },
+
+        { name: 'Fashion Contest', image: './Images/Event/cultural.png' },
+
+        { name: "Gamer's Crusade (Valorant)", image: './Images/Event/cultural.png' },
+
+
     ]
     return (
         <div className='grid grid-cols-1 xs:grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 mb-10 gap-x-3 lg:gap-x-6 gap-y-4'>
