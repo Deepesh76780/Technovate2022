723171cc8272b89416333f340c637162c3ae1ed4
VOLDEMORT-234 <99245631+VOLDEMORT-234@users.noreply.github.com>
2022-10-31 20:37:29 +0530

    Rule_book added

--- src/Mycomps/Events/EventCard.jsx
+++ src/Mycomps/Events/EventCard.jsx
@@ -12,7 +12,8 @@ export const EventCard = ({
   event_posters,
   registration_links,
   CondDirect,
-  DirectURL
+  DirectURL,
+  rule_book,
 }) => {
   const navigate = useNavigate();
 
@@ -20,22 +21,21 @@ export const EventCard = ({
     <div
       className="flex flex-col space-y-3 justify-center items-center bg-black bg-opacity-75 opacity-100 rounded-2xl px-2 py-4 xl:w-[300px] cursor-pointer"
       onClick={() => {
-        if(CondDirect){
+        if (CondDirect) {
           window.open(DirectURL, "_blank");
-        }
-        else{
+        } else {
           navigate(`/eventdetail/${name}`, {
             state: {
               details: {
                 event_name: name,
                 event_detail: details,
                 event_poster: event_poster,
-                rulebook: "",
                 event_heads: event_heads,
                 volunteer_heads: volunteer_heads,
                 registration_link: registration_link,
                 event_posters: event_posters,
-                registration_links: registration_links
+                registration_links: registration_links,
+                rule_book: rule_book,
               },
             },
           });
